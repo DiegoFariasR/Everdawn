8bc8c37b0c98631c312d26721989f5ffb4bc2d2c
DiegoFariasR <DiegoFariasR@users.noreply.github.com>
2026-03-13 16:00:12 +0100

    Refactor battle page: modular components, card layout, unit detail panels, remove keyboard shortcuts

---
+++ BattleSandbox.Web/Battle/BattleHelpers.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using GameCore.Battle;
+
+namespace BattleSandbox.Web.Battle
+{
+    /// <summary>A floating damage/heal number spawned over a unit card.</summary>
+    public record FloatNumber(string UnitId, int Value, bool IsHeal, int OffsetPct, int Key);
+
+    /// <summary>
+    /// Stateless helpers shared by all battle sub-components.
+    /// To add a new damage type: add a case to DmgTypeCss and a .badge-xxx rule in app.css.
+    /// To add a new skill slot style: add a case to SkillSlotClass.
+    /// </summary>
+    public static class BattleHelpers
+    {
+        public static string DmgTypeCss(EffectType t) => t switch
+        {
+            EffectType.Physical => "badge-phys",
+            EffectType.Fire => "badge-fire",
+            EffectType.Cold => "badge-cold",
+            EffectType.Lightning => "badge-lightning",
+            EffectType.Holy => "badge-holy",
+            EffectType.Void => "badge-void",
+            _ => "badge-phys",
+        };
+
+        public static string SkillSlotClass(int index) => index switch
+        {
+            0 => "btn-attack",
+            1 => "btn-skill",
+            _ => "btn-soulburn",
+        };
+    }
+}
